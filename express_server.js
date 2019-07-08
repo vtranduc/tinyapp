f14e1a9ddd7d0cf308d74891675e5c8ce7b63761
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-07-08 23:02:35 +0000

    updated

--- express_server.js
+++ express_server.js
@@ -18,8 +18,32 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  let templateVars = { greeting: 'Hello World!' };
+  
+  // console.log('---------------------');
+  // console.log(req.params.shortURL);
+  // console.log('----------------------')
+
+  res.render("hello_world", templateVars);
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  console.log('---------------------');
+  console.log(req.params.shortURL);
+  console.log(urlDatabase[req.params.ashortURL]);
+  console.log('----------------------')
+
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};// What goes here?
+  //let templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca"};// What goes here?
+
+  res.render("urls_show", templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
