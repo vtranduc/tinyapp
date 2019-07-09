c1fb74137b0b0b0a6f6dab3cf69c293752e5c382
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-07-09 00:46:06 +0000

    udpated

--- express_server.js
+++ express_server.js
@@ -4,10 +4,17 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const generateRandomString = function() {
+
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,9 +34,20 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
