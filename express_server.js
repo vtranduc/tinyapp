82690d93d65d41114e2d193d37343fef116616e3
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-07-10 00:25:19 +0000

    updated

--- express_server.js
+++ express_server.js
@@ -4,6 +4,13 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+//============================
+
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+//============================
+
 const {generateRandomString} = require('./stringGenerator');
 
 const urlDatabase = {
@@ -27,10 +34,39 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 
+//=====================================================================
+
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase };
+  //console.log(templateVars.username)
+  //console.log('Inside of app.get urls/')
   res.render("urls_index", templateVars);
 });
+
+app.post("/login", (req, res) => {
+
+  res.cookie('username',req.body.chosenUsername);
+  // console.log('have set cookies')
+
+  // console.log(req.cookies["username"])
+
+  res.redirect('/urls/')
+});
+
+app.post("/logout", (req, res) => {
+  //res.cookie('username', undefined);
+
+  //console.log('-----------===================-------------------')
+
+  res.clearCookie("username");
+  //console.log('-----------===================-------------------')
+
+  res.redirect('/urls/');
+});
+
+//=====================================================================
 
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
@@ -40,16 +76,20 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new",{generateRandomString: generateRandomString});
+  let templateVars = {username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};// What goes here?
+  let templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]};// What goes here?
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};// What goes here?
+  let templateVars = {username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};// What goes here?
   res.render("urls_show", templateVars);
 });
 
